025e410e487a20310c80d5936a4b5e4ecc56c9c1
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-16 11:13:28 +0100

    bad format

--- webpack.common.js
+++ webpack.common.js
@@ -1,40 +1,40 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  context: path.resolve(__dirname, "src"),
+  context: path.resolve(__dirname, 'src'),
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
-  entry: ["./index.tsx"],
+  entry: ['./index.tsx'],
   output: {
-    filename: "[name].[chunkhash].js",
-    path: path.resolve(__dirname, "./dist"),
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, './dist'),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         exclude: /node_modules/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "index.html",
-      scriptLoading: "blocking",
+      filename: 'index.html',
+      template: 'index.html',
+      scriptLoading: 'blocking',
     }),
     new CleanWebpackPlugin(),
   ],
